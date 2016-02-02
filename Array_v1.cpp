2dcfc57b813fc91293d896a4fe47830cfac0ec41
zincxenon <willbliu94@gmail.com>
2016-02-01 16:31:34 -0800

    v2

--- Array_v1.cpp
+++ Array_v1.cpp
@@ -1,13 +1,12 @@
 // This class holds an integer array of any size, and detects illegal subscripts.
-
-// WARNING - ELEMENTARY EXAMPLE ONLY! THIS VERSION IS NOT SUITABLE FOR ACTUAL USE!
-
+// This version forbids copy and assignment, and so is safe,
+// but you can't use it like a built-in variable.
 
 #include <iostream>
-#include <cstdlib>  // for exit function
+#include <cstdlib>
 using namespace std;
 
-// version 1 - not suitable for actual use
+// version 2 - safe for actual use, but limited
 class Array {
 public:
     // constructor - argument is desired size of the array
@@ -47,102 +46,111 @@ public:
         }
         return ptr[index];
     }
+    // C++11 style for forbidding copy and assignment
+    Array(const Array& source) = delete;
+    Array& operator= (const Array& source) = delete;
     
 private:
+    // C++98 (or C++03) style for forbidding copy and assignment
+    //  Array(const Array& source); // forbid use of copy constructor
+    //  Array& operator= (const Array& source); // forbid use of assignment operator
     int size;
     int* ptr;
 };
 
-void zap();
+void printem(const Array& a); // copy ctor not called, read-only
+double averagem(Array a); // copy ctor called
+void zeroem(Array& a);  // copy ctor not called
+Array squarem(const Array& a);
 
-// demonstrate use of Array Class, version 1
+// demonstrate use of Array Class, version 2
 int main()
 {
+    //  Array x;    // error - no default constructor
     
-    cout << "Enter desired size:";
-    int n;
-    cin >> n;
-    
-    Array my_ints(n);
-    
-    cout << "Enter " << n << " values:" << endl;
-    // size is available from the Array object
-    for (int i = 0; i < my_ints.get_size(); i++) {
-        cout << i << ':';
-        int x;
-        cin >> x;
-        my_ints[i] = x; // use subscript operator on left hand size of assignment
-        // cin >> my_ints[i];   // can also use as input operator variable
-    }
-    
-    for (int i = 0; i < 5; i++) {
-        cout << "Enter an index of number to see, 0 through " << n -1 << " is legal:";
-        int index;
-        cin >> index;
-        int result = my_ints[index]; // use subscript operator on right hand size of assignment
-        cout << "Value at index " << index << " is " << result << endl;
-    }
-    
-    zap();
-    
-    cout << "Done!" << endl;
-    
-}
-
-void zap()
-{
-    cout << "Starting zap" << endl;
     Array a(5);
     for (int i = 0; i < 5; i++)
-        a[i] = i * 5;
-    for (int i = 0; i < 5; i++)
-        cout << a[i] << ' ';
-    cout << endl;
-    cout << "Returning from zap" << endl;
-    return;
+        a[i] = i;   // 0, 1, 2, 3, 4
+    
+    // demonstrate call by const reference
+    cout << "\nContents of a in main:" << endl;
+    printem(a);
+    
+    /*  // demonstrate initialization from copy ctor
+     // error - call to deleted constructor (C++11) / illegal access to private member (C++ 98)
+     Array b(a);
+     cout << "Contents of b after Array b(a);" << endl;
+     printem(b);
+     */
+    /*  // demonstrate call by value
+     cout << "\nAbout to call averagem(a)" << endl;
+     // error - call to deleted constructor (C++11) / illegal access to private member (C++ 98)
+     double avg = averagem(a);
+     cout << "Average is " << avg << endl;
+     cout << "Contents of a after double avg = averagem(a);" << endl;
+     printem(a);
+     */
+    // demonstrate modification through call by reference
+    cout << "\nAbout to call zeroem(a);" << endl;
+    zeroem(a);
+    cout << "Contents of a after zeroem(a);" << endl;
+    printem(a);
+    
+    Array x(5);
+    /*  // demonstrate assignment
+     cout << "\nAbout to execute x = a;" << endl;
+     // error - call to deleted operator= (C++11) / illegal access to private member (C++ 98)
+     x = a;
+     cout << "Contents of a after a = b;" << endl;
+     printem(a);
+     */
+    /*  // demonstrate return value assignment
+     cout << "\nAbout to execute a = squarem(b);" << endl;
+     // error - call to deleted operator= (C++11) / illegal access to private member (C++ 98)
+     x = squarem(a);    // error - illegal access to private member
+     cout << "Contents of a after a = squarem(b);" << endl;
+     printem(a);
+     */
+    cout << "\nDone!" << endl;
+    
 }
 
 
+void printem(const Array& a) // copy ctor not called
+{
+    for (int i = 0; i < a.get_size(); i++)
+        cout << '[' << i << "]:" << a[i] << endl;;
+}
+
+double averagem(Array a) // copy ctor called in client code
+{
+    double sum = 0;
+    
+    for (int i = 0; i < a.get_size(); i++) {
+        sum += a[i];
+        a[i] = 0;   // demo only
+    }
+    cout << "Contents of a in averagem:" << endl;
+    printem(a);
+    return sum/a.get_size();
+}
+
+void zeroem(Array& a)
+{
+    for (int i = 0; i < a.get_size(); i++)
+        a[i] = 0;
+}
 
 
 /*
- Enter desired size:3
- Array of size 3 constructed.
- Enter 3 values:
- 0:0
- 1:10
- 2:20
- Enter an index of number to see, 0 through 2 is legal:2
- Value at index 2 is 20
- Enter an index of number to see, 0 through 2 is legal:1
- Value at index 1 is 10
- Enter an index of number to see, 0 through 2 is legal:0
- Value at index 0 is 0
- Enter an index of number to see, 0 through 2 is legal:2
- Value at index 2 is 20
- Enter an index of number to see, 0 through 2 is legal:2
- Value at index 2 is 20
- Starting zap
- Array of size 5 constructed.
- 0 5 10 15 20
- Returning from zap
- Array of size 5 destroyed.
- Done!
- Array of size 3 destroyed.
- */
-
-/*
- Enter desired size:3
- Array of size 3 constructed.
- Enter 3 values:
- 0:0
- 1:10
- 2:20
- Enter an index of number to see, 0 through 2 is legal:1
- Value at index 1 is 10
- Enter an index of number to see, 0 through 2 is legal:2
- Value at index 2 is 20
- Enter an index of number to see, 0 through 2 is legal:-5
- Attempt to access Array with illegal index = -5
- Terminating program
+ // return a new smart array object
+ Array squarem(const Array& a)
+ {
+ Array b(a.get_size());
+ 
+ for (int i = 0; i < a.get_size(); i++)
+ b[i] = a[i] * a[i];
+ // error - call to deleted constructor (C++11) / illegal access to private member (C++ 98)
+ return b;  // error - illegal access to private member
+ }
  */
